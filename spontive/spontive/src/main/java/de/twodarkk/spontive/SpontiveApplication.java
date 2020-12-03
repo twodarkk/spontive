47ffa99416f17873567aea62e81d06db7a4c8456
twodarkk <simon.lankes@gmx.de>
2020-12-03 19:53:54 +0100

    added files of spontive

---
+++ spontive/spontive/src/main/java/de/twodarkk/spontive/SpontiveApplication.java
@@ -0,0 +1,13 @@
+package de.twodarkk.spontive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpontiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpontiveApplication.class, args);
+	}
+
+}
---
+++ spontive/spontive/src/main/java/de/twodarkk/spontive/database/entity/User.java
@@ -0,0 +1,4 @@
+package de.twodarkk.spontive.database.entity;
+
+public class User {
+}
